6af7f33e2b36b0ba5cca7ca1a194097b87981d26
Hrishikesh Belgaumkar <hribgm2496@gmail.com>
2019-08-06 13:06:35 +0530

    Updates

---
+++ precache-manifest.55b6846a7666c6073307784361ead389.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d3d3787fba60d3bda1a466deaef62d",
+    "url": "/login-react/index.html"
+  },
+  {
+    "revision": "65bfa05f25cc10016e51",
+    "url": "/login-react/static/css/main.75b2bc96.chunk.css"
+  },
+  {
+    "revision": "4f48024d256947cbf9ce",
+    "url": "/login-react/static/js/2.1063ae7b.chunk.js"
+  },
+  {
+    "revision": "65bfa05f25cc10016e51",
+    "url": "/login-react/static/js/main.14f8eab8.chunk.js"
+  },
+  {
+    "revision": "1d2e34b4b130870956be",
+    "url": "/login-react/static/js/runtime~main.6f10b612.js"
+  },
+  {
+    "revision": "317c83ea61eab172f8a1dad69e286ebe",
+    "url": "/login-react/static/media/Ailerons.317c83ea.otf"
+  },
+  {
+    "revision": "0f9bdc30d3db526b02072d19a12cd032",
+    "url": "/login-react/static/media/Blanka.0f9bdc30.otf"
+  }
+]);
